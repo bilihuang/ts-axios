c078735e36235c5ba1d2434f59c3fdb7f9ec8b3d
menghan18 <menghan18@users.noreply.github.com>
2019-09-17 17:45:21 +0800

    feat(/src/helpers): handle response data
    transform response data from string to object

--- src/helpers/data.ts
+++ src/helpers/data.ts
@@ -1,9 +1,22 @@
+// data处理函数
 import { isPlainObject } from './util'
 
 export function transformRequest(data: any): any {
   // 处理body中的对象参数
   if (isPlainObject(data)) {
     return JSON.stringify(data)
+  }
+  return data
+}
+
+// 处理响应的data，转化为json对象
+export function transformResponse(data: any): any {
+  if (typeof data === 'string') {
+    try {
+      data = JSON.parse(data)
+    } catch (e) {
+      // do nothing
+    }
   }
   return data
 }
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,20 @@
-import { AxiosRequestConfig, AxiosPromise } from './types'
+import { AxiosRequestConfig, AxiosPromise, AxiosResponse } from './types'
 import xhr from './xhr'
 import { buildURL } from './helpers/url'
-import { transformRequest } from './helpers/data'
+import { transformRequest, transformResponse } from './helpers/data'
 import { processHeaders } from './helpers/headers'
 
 function axios(config: AxiosRequestConfig): AxiosPromise {
   processConfig(config)
-  return xhr(config)
+  return xhr(config).then(res => {
+    return transformResponseData(res)
+  })
 }
 
 function processConfig(config: AxiosRequestConfig): void {
   config.url = transformURL(config)
   config.headers = transformHeaders(config)
-  config.data = transformResquestData(config)
+  config.data = transformRequestData(config)
 }
 
 function transformURL(config: AxiosRequestConfig): string {
@@ -20,13 +22,18 @@ function transformURL(config: AxiosRequestConfig): string {
   return buildURL(url, params)
 }
 
-function transformResquestData(config: AxiosRequestConfig): any {
+function transformRequestData(config: AxiosRequestConfig): any {
   return transformRequest(config.data)
 }
 
 function transformHeaders(config: AxiosRequestConfig): any {
   const { headers = {}, data } = config
   return processHeaders(headers, data)
+}
+
+function transformResponseData(res: AxiosResponse): AxiosResponse {
+  res.data = transformResponse(res.data)
+  return res
 }
 
 export default axios

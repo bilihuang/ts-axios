96f77004e4a826c2fe7f039bc716e9a41063fc6f
menghan18 <menghan18@users.noreply.github.com>
2019-10-17 17:37:27 +0800

    fix: flatten headers
    flatten headers and merge common and method in config

--- src/helpers/headers.ts
+++ src/helpers/headers.ts
@@ -1,4 +1,5 @@
-import { isPlainObject } from './util'
+import { isPlainObject, deepMerge } from './util'
+import { Method } from '../types'
 
 // 辅助函数，对header中的属性规范化，如content-type转化为Content-Type
 function normalizeHeaderName(headers: any, normalizedName: string): void {
@@ -42,4 +43,21 @@ export function parseHeaders(headers: string): any {
     parsed[key] = val
   })
   return parsed
+}
+
+// 合并headers
+export function flattenHeaders(headers: any, method: Method): any {
+  if (!headers) {
+    return headers
+  }
+
+  headers = deepMerge(headers.common, headers[method], headers)
+
+  const methodsToDelete = ['delete', 'get', 'head', 'options', 'post', 'put', 'patch', 'common']
+
+  methodsToDelete.forEach(method => {
+    delete headers[method]
+  })
+
+  return headers
 }

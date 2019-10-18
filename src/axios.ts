b488af4d9fd26f8bf66509d751b350a757b7a244
menghan18 <menghan18@users.noreply.github.com>
2019-10-18 23:31:27 +0800

    feat: add axios.create
    add axios.create for create new instance of axios

--- src/axios.ts
+++ src/axios.ts
@@ -1,18 +1,24 @@
-import { AxiosInstance, AxiosRequestConfig } from './types'
+import { AxiosRequestConfig, AxiosStatic } from './types'
 import Axios from './core/Axios'
 import { extend } from './helpers/util'
 import defaults from './default'
+import mergeConfig from './core/mergeConfig'
 
 // 创建新的实例并合成混合对象
-function createInstance(config: AxiosRequestConfig): AxiosInstance {
+function createInstance(config: AxiosRequestConfig): AxiosStatic {
   const context = new Axios(config)
   const instance = Axios.prototype.request.bind(context)
 
   extend(instance, context)
 
-  return instance as AxiosInstance
+  return instance as AxiosStatic
 }
 
 const axios = createInstance(defaults)
+
+// axios实例创建函数，可创建新的axios实例并将配置传入
+axios.create = function create(config) {
+  return createInstance(mergeConfig(defaults, config))
+}
 
 export default axios
